535f41d9055355cda9f51e08dc5ae7e8cd67d7f8
Hendra Susanto <anotherhendrasusanto@gmail.com>
2019-05-15 17:46:08 +0700

    chapter8 & chapter9

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,35 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Birdboard</title>
+@extends('layouts.app')
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css" />
-</head>
-<body>
-
-  <div class="container" style="padding-top: 60px;">
-    <h1 class="heading is-1">Create a project</h1>
-    <form method="POST" action="{{ route('projects.store') }}">
-      @csrf
-      <div class="field">
-        <label for="title" class="label">Title</label>
-        <div class="control">
-          <input type="text" class="input" name="title" placeholder="Title">
-        </div>
+@section('content')
+  <h1 class="heading is-1">Create a project</h1>
+  <form method="POST" action="{{ route('projects.store') }}">
+    @csrf
+    <div class="field">
+      <label for="title" class="label">Title</label>
+      <div class="control">
+        <input type="text" class="input" name="title" placeholder="Title">
       </div>
+    </div>
 
-      <div class="field">
-        <label for="description" class="label">Description</label>
-        <div class="control">
-          <textarea name="description" class="textarea"></textarea>
-        </div>
+    <div class="field">
+      <label for="description" class="label">Description</label>
+      <div class="control">
+        <textarea name="description" class="textarea"></textarea>
       </div>
+    </div>
 
-      <div class="field">
-        <button type="submit" class="button is-link">Submit</button>
-      </div>
-    </form>
-  </div>
+    <div class="field">
+      <button type="submit" class="button is-link">Submit</button>
+      <a href="{{ route('projects.index') }}">Cancel</a>
+    </div>
+  </form>
+@endsection
 
-</body>
-</html>
+{{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css" /> --}}
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Document</title>
-</head>
-<body>
-  <h1>Birdboard</h1>
-  @forelse ($projects as $project)
-    <ul>
-      <li>
-        <a href="{{ route('projects.show', $project) }}">
-          {{ $project->title }}
-        </a>
-      </li>
-    </ul>
-  @empty
-    <p>No projects yet.</p>
-  @endforelse
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+  <div class="flex items-center mb-4">
+    <h1 class="mr-auto">Birdboard</h1>
+
+    <a href="{{ route('projects.create') }}">Create New Project</a>
+  </div>
+
+  <div class="flex flex-wrap">
+    @forelse ($projects as $project)
+      <div class="w-full sm:w-1/2 md:w-1/3 bg-white mb-4 mr-4 rounded shadow p-5" style="height: 200px;">
+        <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+
+        <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+      </div>
+    @empty
+      <p>No projects yet.</p>
+    @endforelse
+  </div>
+@endsection
